ffdd3a0fed6ec741b864b790d3c31da54890beea
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-03-15 18:59:10 +0100

    completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,44 @@
 $(document).ready(function(){
   // La pagina espone 5 numeri generati casualmente.
   var serie = generaSerie(5, 1, 100);
-  var finestra = $('.numeri p');
+  var finestra = $('.numeri p.1');
   finestra.text(serie);
 
   // Da li parte un timer di 30 secondi. Poi nascondo i numeri
-  var user = setTimeout(chiudi, 2000);
+  setTimeout(chiudi, 2000);
 
   // l'utente deve inserire, uno alla volta, i numeri che ha visto
   // precedentemente, tramite il prompt().
 
+  setTimeout(bottoneShow, 2000);
 
+
+  var bottone = $('button');
+
+
+  bottone.click(function(){
+    $(this).hide();
+    var user = [];
+    while (user.length < 5) {
+      var nrUser = parseInt(prompt('Scrivi, uno per volta, i numeri che ricordi:'));
+      if (!isNaN(nrUser) && !user.includes(nrUser)) {
+        user.push(nrUser);
+      } else {
+        return user.length
+      }
+    };
+    var giusti = [];
+    for (var i = 0; i < user.length; i++) {
+      if (serie.includes(user[i])) {
+        giusti.push(user[i]);
+      }
+    }
+    var punteggio = giusti.length;
+
+    $('.numeri p.1').text('HAI INDOVINATO ' + punteggio + ' NUMERI')
+    $('.numeri p.2').text(giusti);
+    $('.numeri').show();
+  })
 });
 
 function generaSerie(serieLung, min, max){
@@ -21,7 +49,6 @@ function generaSerie(serieLung, min, max){
       serie.push(serieNr);
     }
   }
-
   return serie;
 }
 
@@ -30,25 +57,22 @@ function randomNumberInRange(min, max) {
 }
 
 function chiudi(){
-  var finestra = $('.numeri p');
+  var finestra = $('.numeri');
 
   finestra.hide();
-
-  setTimeout(game, 2000);
-
 }
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-function game(){
-  var user = [];
 
-  while (user.length < 5) {
-    var nrUser = parseInt(prompt('Scrivi, uno per volta, i numeri che ricordi:'));
-    if (!isNaN(nrUser) && !user.includes(nrUser)) {
-      user.push(nrUser);
-    } else {
-      return user.length
-    }
-  };
-  console.log(user);
-  return user;
+function bottoneShow(){
+  var bottone = $('button');
+  bottone.addClass('active');
 }
+
+// var controllo = setTimeout(controllo, 6000)
+//
+//
+// function controllo(){
+//   console.log(user);
+// }
+
+// function confronto(serie, user){
+// }
